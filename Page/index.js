dea8bc0698345708811cd60d2b9c9cd0949acd77
Felix Freidman <felixfreidman@gmail.com>
2021-10-11 18:33:10 +0800

    Block update

---
+++ Page/index.js
@@ -0,0 +1,5 @@
+// var link = document.getElementById("nextPageBtn");
+// console.log(link);
+// link.addEventListener("click", () => {
+//     link.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+// });
